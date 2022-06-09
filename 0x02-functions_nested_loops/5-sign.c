b628bacc41304ee6420185d8ae123482086e224c
ibekwe adaeze <mariam22.ibekwe@gmail.com>
2022-06-08 17:54:16 -0700

    t push

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - a function that prints the sign of a number
  * @n: number input
  * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero*
+ * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
@@ -19,7 +19,8 @@ else if (n == 0)
 _putchar(48);
 return (0);
 }
-else if (n < 0
+else if (n < 0)
+{
 _putchar('-');
 }
 return (-1);
