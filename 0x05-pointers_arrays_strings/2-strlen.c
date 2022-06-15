883d524a690d87166880b9fee6bb66dbedeca4bf
ibekwe adaeze <mariam22.ibekwe@gmail.com>
2022-06-14 21:10:04 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate length of string
+ * @s: parameter
+ *
+ *
+ * Return: length of string.
+ */
+
+int _strlen(char *s)
+{
+int myLen = strlen(s);
+
+return (myLen);
+}
